1d7edbbb38088322630a60894016163734663847
tanakrid chanburi <tanakrid.c@ku.th>
2021-08-02 14:25:58 +0700

    test dependency case

--- Demo01/src/main/java/unittest/Hello.java
+++ Demo01/src/main/java/unittest/Hello.java
@@ -2,7 +2,24 @@ package unittest;
 
 public class Hello {
 
+    public UserDB userDB;
+
     public String hi(String name) {
         return "Hello, " + name;
+    }
+
+    public String workWithDb(int id) {
+        return userDB.getNameByID(id);
+    }
+}
+
+class UserDB {
+    public String getNameByID(int id) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        throw new RuntimeException("DB Fail");
     }
 }
--- Demo01/src/test/java/unittest/HelloTest.java
+++ Demo01/src/test/java/unittest/HelloTest.java
@@ -14,4 +14,34 @@ class HelloTest {
         assertEquals("Hello, chai", actualResult);
     }
 
+    @Test
+    @DisplayName("test database independant")
+    public void case02() {
+        Hello hello = new Hello();
+        hello.userDB = new UserDB(){
+            @Override
+            public String getNameByID(int id) {
+                return "chai";
+            }
+        };
+        String name = hello.workWithDb(1);
+        assertEquals("chai", name);
+    }
+
+    @Test
+    @DisplayName("เกิด exception เมื่อค้นหาผู้ใช้งาน id=2 ไม่เจอ")
+    public void case03() {
+        Hello hello = new Hello();
+        hello.userDB = new UserDB(){
+            @Override
+            public String getNameByID(int id){
+                throw new UserNotFoundException("Id=" + id + " not found");
+            }
+        };
+        // Junit 5 + Exception
+        Exception exception = assertThrows(UserNotFoundException.class, () ->
+                hello.workWithDb(2));
+        assertEquals("Id=2 not found", exception.getMessage());
+    }
+
 }
