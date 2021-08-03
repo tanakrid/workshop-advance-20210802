4985c605da42ffa85094547743cf08414fa9755b
tanakrid chanburi <tanakrid.c@ku.th>
2021-08-03 14:30:37 +0700

    test stub and spy

--- Demo01/src/main/java/testdouble/GenerateIdService.java
+++ Demo01/src/main/java/testdouble/GenerateIdService.java
@@ -3,8 +3,14 @@ package testdouble;
 import java.util.Random;
 
 public class GenerateIdService {
+
+    private Random random = new Random();
+
+    public void setRandom(Random random) {
+        this.random = random;
+    }
+
     public String get() {
-        Random random = new Random(); // Dependency
         int number = random.nextInt(10);
         String id = "XYZ" + number;
         return id;
--- Demo01/src/test/java/testdouble/GenerateIdServiceTest.java
+++ Demo01/src/test/java/testdouble/GenerateIdServiceTest.java
@@ -3,6 +3,8 @@ package testdouble;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GenerateIdServiceTest {
@@ -13,11 +15,44 @@ class GenerateIdServiceTest {
         - Repeat (Not Ok)
      */
 
+
     @Test
-    @DisplayName("ID = XYZ7")
+    @DisplayName("Demo Stub Random: ID = XYZ7")
     public void case01() {
         GenerateIdService service = new GenerateIdService();
+        Random random = new Random(){
+            @Override
+            public int nextInt(int bound) {
+                return 7;
+            }
+        };
+        service.setRandom(random);
         String id = service.get();
         assertEquals("XYZ7", id);
+    }
+
+    @Test
+    @DisplayName("Demo Spy Random: Called nextInt(int bound)")
+    public void case02() {
+        GenerateIdService service = new GenerateIdService();
+        SpyRandom random = new SpyRandom();
+        service.setRandom(random);
+        String id = service.get();
+        random.verify(1);
+    }
+}
+
+class SpyRandom extends Random {
+    private int count = 0;
+    @Override
+    public int nextInt(int bound) {
+        this.count++;
+        return super.nextInt(bound);
+    }
+
+    public void verify(int expectedCount) {
+        if(expectedCount != count) {
+            fail("จำนวนการเรียกใช้งาน method nextInt() ไม่ถูกต้อง");
+        }
     }
 }
